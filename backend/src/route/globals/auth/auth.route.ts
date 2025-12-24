07fe60d53ff134565151070b1579903b634f42d0
rajiv <rajiv.sapkota96@gmail.com>
2025-12-24 21:34:54 +0545

    created global register route

---
+++ backend/src/route/globals/auth/auth.route.ts
@@ -0,0 +1,11 @@
+
+import express, {Router} from 'express'
+import  AuthController  from '../../../controller/globals/auth/auth.controller'
+
+const router:Router =express.Router()
+
+
+router.route('/register-user').post(AuthController.registerUser).post(AuthController.loginUser)
+
+
+export default router
